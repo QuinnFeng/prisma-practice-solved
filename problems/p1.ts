5eae931bc101ab90ec336e107e4009be0e9b5cc1
QuinnFeng <quinnfeng26276@gmail.com>
2024-04-29 21:50:53 -0400

    first commit

--- problems/p1.ts
+++ problems/p1.ts
@@ -1,5 +1,18 @@
-import { prisma } from "./prisma";
+import { Prisma, PrismaClient } from '@prisma/client'
+import { error } from 'console';
+
+const prisma = new PrismaClient();
 
 // Hint: look up "orderBy"
 // get an array of all users
-export const getAllUsers = () => {};
+
+export const getAllUsers = async () => {
+    const users = await prisma.user.findMany({ orderBy: [
+        {
+          username: 'asc',
+        }
+      ]});
+    return users;
+};
+
+// getAllUsers().then(result=>console.log(result)).catch(error=>console.error(error));
